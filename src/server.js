4addae012752bc95b6423cd281716de1af23a9b3
Alessandro Di Luca <email.recuperoxearth@gmail.com>
2026-03-10 10:32:35 +0100

    Created home page and css stylesheets

--- src/server.js
+++ src/server.js
@@ -4,12 +4,37 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../public/pages/html'));
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 
+// Home and game
+app.get("/home", (req, res) => {
+    res.render("home", {
+        titolo: "2048",
+        gridSize: 4,
+        score: 0,
+        record: 8192,
+        moves: 0,
+        playing: false,
+        grid: Array(16).fill(0),
+        leaderboard: [
+
+        ],
+        userLoggedIn: true,
+        user: {
+
+        }
+    });
+});
+
+
+// Listen on port 3000
 app.listen(PORT, () => {
     console.log(`Server avviato su http://localhost:${PORT}`);
 });
-
 
 // Fallback 404
 app.use((req, res) => {
